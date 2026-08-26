bf5659b4aec054c14fc0c6995ee57df1ed74628f
The ml guy <unknowninsaa1@gmail.com>
2026-08-27 00:50:07 +0530

    Update and rename 13_SVM Example.py to 13_Using_SVM_in_Sklearn_Dataset.py

--- 13_Using_SVM_in_Sklearn_Dataset.py
+++ 13_Using_SVM_in_Sklearn_Dataset.py
@@ -1,6 +1,5 @@
 # Using the SVM algorithm in the Sklearn Breast cancer Dataset.
 # Import necessary libraries
-# in
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
